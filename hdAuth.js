4c20179f4e04d5151ad740403dbaec48b3fff48c
Andy Flury <andrewflury@berkeley.edu>
2022-07-07 15:12:18 -0700

    Password input and call started

---
+++ hdAuth.js
@@ -0,0 +1,23 @@
+
+
+$('input').on('keypress', async (e) => {
+    // e.preventDefault();
+    var code = e.keyCode || e.which;
+    if(code==13){
+        const pwd = {
+            'pwd': $('input').val()
+        };
+        if (pwd['pwd'].length == 0) {
+            return;
+        }
+        let response = await fetch(HD_AUTHORIZE_API, {
+            method: 'POST',
+            body: JSON.stringify(pwd)
+          });
+        response
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
+    }
+});
